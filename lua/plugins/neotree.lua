9adc32909a485e827ee76782e6ce4a3cd1dd04df
invader237 <diego.trivino.fr@gmail.com>
2024-05-20 18:33:33 +0200

    :sparkles:(neotree.lua): added keyboard shortcut to display git status

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,6 +7,7 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<C-b>", ":Neotree filesystem toggle left<CR>", {})
+        vim.keymap.set("n", "<C-g>", ":Neotree float git_status<CR>", {})
 	end,
 }
