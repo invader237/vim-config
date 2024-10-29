76d7e2d120a82bf475d783f5be3a8de00d726757
invader237 <diego.trivino.fr@gmail.com>
2024-10-29 16:19:11 +0100

    :zap:(lsp-config.lua): server list sharing

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,18 +1,31 @@
+local servers = {
+	"autopep8",
+	"bash-language-server",
+	"clangd",
+	"cpplint",
+	"cpptools",
+	"css-lsp",
+	"html-lsp",
+	"js-debug-adapters",
+	"json-lsp",
+	"lua-language-server",
+	"phpactor",
+	"phpmd",
+	"prettyphp",
+	"python-lsp-server",
+	"stylelint-lsp",
+	"stylua",
+	"typescript-language-server",
+	"ts-standard",
+	"yaml-language-server",
+}
+
 return {
 	{
 		"williamboman/mason.nvim",
 		lazy = false,
 		opt = {
-			ensure_installed = {
-				"typescript-language-server",
-				"ts-standard",
-				"lua-language-server",
-				"emmet-language-server ",
-				"html-lsp ",
-				"css-lsp ",
-				"stylua",
-				"clangd",
-			},
+			ensure_installed = servers,
 		},
 
 		config = function()
@@ -33,12 +46,13 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
+
+			for _, server in ipairs(servers) do
+				lspconfig[server].setup({
+					capabilities = capabilities,
+					-- Autres options spécifiques au serveur peuvent être ajoutées ici
+				})
+			end
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
