f52793076cd1e32fc2db2a42f8b7a58d3b81852f
invader237 <diego.trivino.fr@gmail.com>
2024-11-05 16:50:16 +0100

    :sparkles:(floaterm.lua): New term pluggin

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,8 +1,20 @@
 return {
-    'voldikss/vim-floaterm',
-    config = function()
-        vim.keymap.set('n', 'fn', ':FloatermNew<CR>')
-        vim.keymap.set('n', '<F12>', ':FloatermToggle<CR>')
-    end
-
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				direction = "float",            -- Utilise le mode flottant
+				size = 15,                      -- Hauteur souhaitée pour le terminal
+				open_mapping = [[<c-t>]],       -- Raccourci pour ouvrir et fermer le terminal
+				float_opts = {
+					border = "curved",         -- Style de bordure (ex: "curved", "single")
+					width = math.floor(vim.o.columns * 0.2),   -- Largeur à 40% de l'écran
+					height = 15,               -- Hauteur du terminal flottant
+					row = vim.o.lines - 20,    -- Position verticale (proche du bas)
+					col = 0,                   -- Position à gauche
+				},
+			})
+		end,
+	},
 }
